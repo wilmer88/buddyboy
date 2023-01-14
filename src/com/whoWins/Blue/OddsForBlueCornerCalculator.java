ca946d539d6f05c27390cb2dfcc59c849d1d933f
Wilmer Rivera <wilmerbaby@gmail.com>
2023-01-14 14:21:11 -0500

    created oddsCalculator for red and blue corner to return point based on the the given odds

--- src/com/whoWins/Blue/OddsForBlueCornerCalculator.java
+++ src/com/whoWins/Blue/OddsForBlueCornerCalculator.java
@@ -1,4 +1,19 @@
 package com.whoWins.Blue;
 
 public class OddsForBlueCornerCalculator {
+    Double BlueOddsCalculator(Integer oddsForBlue){
+        if(oddsForBlue <= -1000){
+            return 10.0;
+        } else if (oddsForBlue <= -800) {
+            return 8.0;
+        }else if (oddsForBlue <= -600) {
+            return 6.0;
+        }else if (oddsForBlue <= -400) {
+            return 4.0;
+        }
+        else if (oddsForBlue <= -200) {
+            return 2.0;
+        }
+        return 1.0;
+    }
 }
--- src/com/whoWins/Red/OddsForRedCalculator.java
+++ src/com/whoWins/Red/OddsForRedCalculator.java
@@ -1,4 +1,19 @@
 package com.whoWins.Red;
 
 public class OddsForRedCalculator {
+    Double redOddsCalculator(Integer oddsForRed){
+        if(oddsForRed <= -1000){
+            return 10.0;
+        } else if (oddsForRed <= -800) {
+            return 8.0;
+        }else if (oddsForRed <= -600) {
+            return 6.0;
+        }else if (oddsForRed <= -400) {
+            return 4.0;
+        }
+        else if (oddsForRed <= -200) {
+            return 2.0;
+        }
+        return 1.0;
+    }
 }
